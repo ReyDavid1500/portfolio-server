75298913d3f0dabc104856b3fd6c58305c15166b
David Guzman <davidguzman1500@gmail.com>
2023-12-18 10:17:50 -0300

    resend service implemented

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv")
 const mongoose = require("mongoose");
-const Message = require("./models/message");
+const { Resend } = require("resend")
 
 const app = express();
-dotenv.config(); // Hacer configuración! 
+const resend = new Resend("re_YoP6VaTh_MvSLWE5xbnMJxh53isWMSFzq")
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const CONNECTION = process.env.CONNECTION;
 
-console.log(CONNECTION)
-
-mongoose.connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION)
     .then(() => console.log("Connected to DB"))
     .catch(error => console.log("Connection Error ", error))
 
@@ -21,21 +20,24 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.post("/", async (req, res) => {
-    try {
-        const message = new Message({
-            name: req.body.name,
-            email: req.body.email,
-            message: req.body.message,
+app.post('/contact', (req, res) => {
+    const { from, to, subject, html } = req.body;
+
+    resend.emails
+        .send({
+            from,
+            to,
+            subject,
+            html,
         })
+        .then(() => {
+            res.json({ success: true });
+        })
+        .catch((error) => {
+            console.error('Failed to send email:', error);
+            res.json({ success: false, error: error.message });
+        });
+});
 
-        await message.save();
-
-        res.json(message).end();
-    } catch (error) {
-        console.log("Error message: ", error)
-        res.status(500)
-    }
-})
 
 app.listen(PORT, () => console.log("Server OK in port " + PORT))
