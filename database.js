c7c973e5f808bc2b435ba233c35f9ca3a5178f9a
David Guzman <davidguzman1500@gmail.com>
2024-05-14 13:37:20 -0400

    railway deployment PORT test

--- database.js
+++ database.js
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE_NAME,
+    port: process.env.DB_PORT,
 }).promise();
 
 export async function getCertificates() {
